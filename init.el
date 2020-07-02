bdb71ba8ba33874c7dc0927290843e66bac0fc90
debanjum <debanjum@gmail.com>
2020-07-01 23:26:14 -0700

    Add dumb jump for generic code navigation

--- init.el
+++ init.el
@@ -198,6 +198,16 @@
                  (setq undo-tree-visualizer-timestamps t)
                  (setq undo-tree-visualizer-diff t)))
 
+(use-package dumb-jump
+  :ensure t
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g b" . dumb-jump-back)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
+)
 
 ;; Uniquify buffer names foo.c:src, foo.c:extra
 (require 'uniquify)
