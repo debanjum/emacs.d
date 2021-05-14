db7b2f36174873dcb541d47023949f7cb5ae4274
Debanjum Singh Solanky <debanjum@gmail.com>
2021-05-13 21:34:34 -0700

    Remove PDF Tools (for now). Integrating with Mac maybe an issue

--- init.el
+++ init.el
@@ -373,26 +373,6 @@
              org-noter-auto-save-last-location t
              org-noter-notes-search-path '("~/Notes"))))
 
-(use-package pdf-tools
- :pin manual ;; manually update
- :config
- ;; initialise
- (pdf-tools-install)
- ;; open pdfs scaled to fit page
- (setq-default pdf-view-display-size 'fit-page)
- ;; automatically annotate highlights
- (setq pdf-annot-activate-created-annotations t)
- ;; use normal isearch
- (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward)
- ;; turn off cua so copy works
- (add-hook 'pdf-view-mode-hook (lambda () (cua-mode 0)))
- ;; more fine-grained zooming
- (setq pdf-view-resize-factor 1.1)
- ;; keyboard shortcuts
- (define-key pdf-view-mode-map (kbd "h") 'pdf-annot-add-highlight-markup-annotation)
- (define-key pdf-view-mode-map (kbd "t") 'pdf-annot-add-text-annotation)
- (define-key pdf-view-mode-map (kbd "D") 'pdf-annot-delete))
-
 (use-package org-randomnote
   :ensure t
   :bind ("C-c r" . org-randomnote)
