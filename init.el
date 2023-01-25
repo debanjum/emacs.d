9740eabd883764c3758cc10433d6ebf6ff67c2ff
Debanjum Singh Solanky <debanjum@gmail.com>
2023-01-25 15:31:18 -0300

    Suppress undo tree save, load history messages in echo area. Too noisy

--- init.el
+++ init.el
@@ -689,10 +689,29 @@
   :chords (("uu" . undo-tree-visualize))
   :diminish undo-tree-mode
   :config (progn (global-undo-tree-mode 1)
-                 (setq undo-tree-visualizer-timestamps t)
-                 ;; Prevent undo tree files from polluting directories with undo-tree history
-                 (setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo-tree")))
-                 (setq undo-tree-visualizer-diff t)))
+                 (setq
+                  undo-tree-visualizer-timestamps t
+                  ;; Prevent undo tree files from polluting directories with undo-tree history
+                  undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo-tree"))
+                  undo-tree-visualizer-diff t)
+
+                 ;; Suppress the message saying that the undo history file was
+                 ;; saved (because this happens every single time you save a file).
+                 ;; borrowed from raxod502's radian emacs config
+                 (defun radian--undo-tree-suppress-undo-history-saved-message
+                     (undo-tree-save-history &rest args)
+                   (let ((inhibit-message t))
+                     (apply undo-tree-save-history args)))
+                 (advice-add #'undo-tree-save-history :around #'radian--undo-tree-suppress-undo-history-saved-message)
+
+                 ;; Suppress the message saying that the undo history could not be
+                 ;; loaded because the file changed outside of Emacs.
+                 (defun radian--undo-tree-suppress-buffer-modified-message
+                     (undo-tree-load-history &rest args)
+                   (let ((inhibit-message t))
+                     (apply undo-tree-load-history args)))
+                 (advice-add #'undo-tree-load-history :around #'radian--undo-tree-suppress-buffer-modified-message)
+                 ))
 
 (use-package dumb-jump
   :bind (("M-g o" . dumb-jump-go-other-window)
